64f9c05dfe25efcc4e1298aa27a9f80046f5f1a9
Walleks Miranda <walleksmiranda@gmail.com>
2020-12-06 19:42:50 -0300

    config [debug-vscode, eslint, prettier]

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,11 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({ message: 'hello world' }));
+routes.post('/user', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = { name, email };
+  return response.json(user);
+});
 
 export default routes;
